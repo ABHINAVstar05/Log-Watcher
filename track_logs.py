7c4fe8b5f04b2c78987f250f825cc40094c9066d
Abhinav <kumar.abhinav3125@gmail.com>
2023-09-18 17:58:17 +0530

    Applied PEP8 coding standard

--- track_logs.py
+++ track_logs.py
@@ -6,37 +6,40 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(message)s',
+                    format='[+] %(asctime)s - %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S %Z')
 
 class FileWatcherBinding:
+    """ a class to show blueprint of the Log Watcher attributes and methods (functionalities) """
 
     def __init__(self, sampling_frequency) -> None:
         self.sampling_frequency = sampling_frequency
         self.observer = Observer()
 
-    def watch_file_dir(self, file_name, file_dir_event_handler) -> None:
-        
+    def watch_file_dir(self, event_handler) -> None:
+        """ a function to start the observer on various file system events """
+
         class Helper(FileSystemEventHandler):
+            """ a helper function to utilize FileSystemEventHandler functionalities """
 
             def on_created(self, event):
                 val = info(event.src_path)
                 key = 1
-                file_dir_event_handler(val[0], val[2], val[1], key)
+                event_handler(val[0], val[2], val[1], key)
 
-            def on_deleted(self, event):  
+            def on_deleted(self, event):
                 val = info(event.src_path)
                 key = 2
-                file_dir_event_handler(val[0], val[2], val[1], key)
+                event_handler(val[0], val[2], val[1], key)
 
             def on_modified(self, event):
                 val = info(event.src_path)
                 key = 3
-                file_dir_event_handler(val[0], val[2], val[1], key)
+                event_handler(val[0], val[2], val[1], key)
 
-        event_handler = Helper()
+        handler = Helper()
         path = "."
-        self.observer.schedule(event_handler, path, recursive = True)
+        self.observer.schedule(handler, path, recursive = True)
         self.observer.start()
 
         try:
@@ -48,19 +51,23 @@ class FileWatcherBinding:
 
 
 def file_dir_event_handler(file_type, file_name, src_dir, key) -> None:
+        """ function to log file and directory events """        
+
         if key == 1:
-            logging.info(f"'{file_type}' '{file_name}' is created at location '{src_dir}'")
+            logging.info(f"{file_type} - {file_name} - created at - {src_dir}")
 
         elif key == 2:
-            logging.info(f"'{file_name}' is deleted at location '{src_dir}'")
+            logging.info(f"{file_name} - deleted at - {src_dir}")
 
         elif key == 3:
-            logging.info(f"'{file_type}' '{file_name}' is modified at location '{src_dir}'")
+            logging.info(f"{file_type} - {file_name} - modified at - {src_dir}")
 
         else:
             logging.error("Unknown behavior occured")
 
 def info(s) -> () :
+    """ function to extract info from event.src_path passed as string s as an argument """
+
     file_type = None
     if os.path.isdir(s):
         file_type = 'Directory'
@@ -74,13 +81,13 @@ def info(s) -> () :
 if __name__ == "__main__":
 
     print("[+] Tracking logs...\n")
-   
+
     SAMPLING_FREQUENCY = 5.0
 
     file_watcher = FileWatcherBinding(SAMPLING_FREQUENCY)
 
-    FILE_NAME = "."
+    #FILE_NAME = "."
 
-    file_watcher.watch_file_dir(FILE_NAME, file_dir_event_handler)
+    file_watcher.watch_file_dir(file_dir_event_handler)
 
     print("\nObserver stopped!")
